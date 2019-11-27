699126bc8d94002b6cb7d3a1c829ba8a30c17cf9
Anzhela Martsinovich <anzhela.martsinovich@gmail.com>
2019-11-27 22:03:42 +0300

    refactor: change structure of the project

--- projects/main.js
+++ projects/main.js
@@ -1,4 +1,4 @@
-let change = document.querySelector('.change');
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-	change.style.display = "none";
-}
+// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+// 	let change = document.querySelector('.change');
+// 	change.style.display = "none";
+// }
